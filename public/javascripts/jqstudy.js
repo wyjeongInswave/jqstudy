b8e1f54ac2344da631f4ce32bd22cd8f8e86ade9
OpenShift guest <feelblu@gmail.com>
2013-06-03 00:30:05 -0400

    add highlight.js

--- public/javascripts/jqstudy.js
+++ public/javascripts/jqstudy.js
@@ -1,8 +1,8 @@
  $().ready(function() {
-  $("xmp.run").each(function(idx, el) {
+  $("pre.run code").each(function(idx, el) {
     var button = $("<input class='run' type='button' value='실행하기' />").click(function() {
-      $.globalEval($(el).html());
+      $.globalEval($(el).text());
     });
-    $(el).wrap("<div class='source' />").after(button);
+    $(el).after(button);
   });
 });
